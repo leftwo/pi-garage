627b5089d553b3852783dc2a37d2896dbb7501f7
Alan Hanson <leftwo@gmail.com>
2014-12-01 23:22:30 -0800

    Basic layout of program started

--- gdoor.py
+++ gdoor.py
@@ -1,15 +1,34 @@
-#
+#!/usr/bin/python
 # Requires RPi.GPIO
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-while True:
-    if (GPIO.input(23) == 1):
-        print("Button 1 pressed")
-    else:
-	print("Button 1 released")
+# XXX Move constants to config file, import that.
+# XXX Add logging to console.
 
-    sleep(1)
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+
+def get_state():
+    """ Read the state of the door switch """
+    if (GPIO.input(23) == 1):
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    setup()
+    state = get_state()
+
+    print "Starting with state ", state
+
+    while True:
+        new_state = get_state()
+        if (new_state != state):
+	    print "state change to ", new_state
+	    state = new_state
+ 
+        sleep(1)
 
